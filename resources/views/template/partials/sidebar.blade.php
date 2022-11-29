814d93dc32f70aa31c1b2b59c5724b10a0373007
annisa54 <anisamujamalah.com>
2022-11-29 17:41:35 +0700

    tugas baru anisa

--- resources/views/template/partials/sidebar.blade.php
+++ resources/views/template/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
           <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Yesica Putri Purba</a>
+          <a href="#" class="d-block">Anisa</a>
         </div>
       </div>
 
